3edfc0095aa8f9599455b91b3e51ddb30618c639
hrfeels <hrfeels@7f9ead38-de1e-18c6-a1f4-6645cdb958a2>
2010-11-27 23:40:34 +0000

    initial add of 'demo', just added to toolbar no change listener.

---
+++ JHorizontalSpinnerDemo.java
@@ -0,0 +1,45 @@
+package horizontalSpinner;
+
+
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JSpinner;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
+
+
+public class JHorizontalSpinnerDemo {
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new JHorizontalSpinnerDemo();
+            }
+        });
+    }
+
+        public JHorizontalSpinnerDemo(){
+        //initialize the screen etc:
+        JFrame frame = new JFrame("JHorizontalSpinner Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        JToolBar toolBar = new JToolBar();
+        toolBar.setLayout(new FlowLayout());
+        JHorizontalSpinner horizontalSpinner = new JHorizontalSpinner();
+       // horizontalSpinner.setPreferredSize(new Dimension(150, horizontalSpinner.getPreferredSize().height));
+        JSpinner verticalSpinner = new JSpinner();
+        toolBar.add(horizontalSpinner);
+        toolBar.addSeparator();
+        toolBar.add(verticalSpinner);
+
+        frame.add(toolBar, BorderLayout.SOUTH);
+       // frame.add()
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+}
